1b3186a247b5b70b680b6836266f92f22e2e1459
cindyxu <serinaxu@163.com>
2018-07-30 12:26:12 +0800

    vue axios

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <v-header></v-header>
+    <v-header :seller="seller"></v-header>
     <div class="tab border-1px">
       <div class="tab-item">
         <router-link to="/goods">商品</router-link>
@@ -19,8 +19,24 @@
 <script>
   import header from 'components/header/header'
 
+  const ERR_OK = 0
+
   export default {
     name: 'App',
+    data() {
+      return {
+        seller: {}
+      }
+    },
+    created() {
+      this.axios.get('/api/seller', {}).then(response => {
+        response = response.data
+        if (response.errno === ERR_OK) {
+          this.seller = response.data
+          console.log(this.seller)
+        }
+      })
+    },
     components: {
       'v-header': header
     }
--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+Vue.use(VueAxios, axios)
 
 Vue.config.productionTip = false
 
@@ -10,6 +14,6 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
-  components: { App },
+  components: {App},
   template: '<App/>'
 })
